5bf7582f882cee9580375670e57987a345040c69
Anicet Serge <tghil7@aol.com>
2018-01-27 12:52:21 -0600

    Separated the model from the view model in knockout app

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
-var ViewModel = function(){
+var Cat = function (){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Felix');
 	this.imgSrc = ko.observable('img/Felix.jpg');
 	var infant = "infant";
 	var teen = "teen";
-	
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
-	};
-	
+	this.nicknames = ko.observableArray([{nickname:"Minet"}, 
+										{nickname:"Francois"}, 
+										{nickname:"Piper"}
+					]);
 	this.level = ko.computed(function(){
-		if (this.clickCount() < 10 || this.clickCount() == 10 )
+	if (this.clickCount() < 10 || this.clickCount() == 10 )
 		  return infant;
-		else if (this.clickCount() > 10 && this.clickCount() < 20)
+	else if (this.clickCount() > 10 && this.clickCount() < 20)
 		  return teen;
 		}, this);
+						
+	
+}
+
+
+
+
+var ViewModel = function(){
+	this.currentCat = ko.observable(new Cat());
+   
+   this.incrementCounter = function(){
+	this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
 }
 
 ko.applyBindings(new ViewModel());
