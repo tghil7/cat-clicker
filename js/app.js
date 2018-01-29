edc1f6760419474c9e1f68050bec56a4b76105ce
Anicet Serge <tghil7@aol.com>
2018-01-28 18:57:05 -0600

    Updated knockout app to include 2 more cats

--- js/app.js
+++ js/app.js
@@ -1,31 +1,63 @@
-var Cat = function (){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Felix');
-	this.imgSrc = ko.observable('img/Felix.jpg');
+var Cat = function (data){
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
 	var infant = "infant";
 	var teen = "teen";
-	this.nicknames = ko.observableArray([{nickname:"Minet"}, 
-										{nickname:"Francois"}, 
-										{nickname:"Piper"}
-					]);
+	var adult = "adult";
+	this.nicknames = ko.observableArray(data.nicknames);
 	this.level = ko.computed(function(){
 	if (this.clickCount() < 10 || this.clickCount() == 10 )
 		  return infant;
-	else if (this.clickCount() > 10 && this.clickCount() < 20)
+	else if (this.clickCount() > 10 && this.clickCount() < 18)
 		  return teen;
+    else if (this.clickCount()== 18 || this.clickCount() > 18)
+	      return adult;
 		}, this);
 						
 	
 }
 
+var initialCats = [
+   {
+	clickCount :0,
+	name :"Felix",
+	imgSrc :"img/Felix.jpg",
+    nicknames: ['Tabby', 'Minet', 'Gregoire', ]
+   },
+   {
+	clickCount :0,
+	name :"Chewie",
+	imgSrc :"img/Chewie.jpg",
+    nicknames: ['Isaac', 'Sam', 'Titus', ]   
+   },
+   {
+    clickCount :0,
+	name :"Frisky",
+	imgSrc :"img/Frisky.jpg",
+    nicknames: ['Piper', 'Titeuf', 'Firmin', ]
+   }
+]
+
 
 
 
 var ViewModel = function(){
-	this.currentCat = ko.observable(new Cat());
+	var self =this;
+	this.catList = ko.observableArray([]);
+	
+	initialCats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+		});
+		
+	this.currentCat = ko.observable(this.catList()[0]);
+		
+	this.updateCat = function(clickedCat){
+			self.currentCat(clickedCat);	
+	};	
    
    this.incrementCounter = function(){
-	this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	this.clickCount(this.clickCount() + 1);
 	};
 }
 
